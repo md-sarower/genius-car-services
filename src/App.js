ae768832dab51db46d21db4fec7fcc8f236b0ca9
md-sarower <sarower9415@gmail.com>
2022-05-13 01:49:12 +0600

    Single service page done from my pc

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import About from './pages/about/About';
+import Home from './pages/home/services/home/Home';
+import Header from './pages/shared/header/Header';
+import Footer from './pages/shared/footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
